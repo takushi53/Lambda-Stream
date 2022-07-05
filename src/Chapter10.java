2802f20337f301d07349d4166d618d642d0a8638
takuya morishita <102013434+takushi53@users.noreply.github.com>
2022-07-06 07:22:38 +0900

    課題提出

---
+++ src/Chapter10.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+public class Chapter10 {
+	public static void main(String[] args) throws Exception {
+		List<Task> list = new ArrayList<>();
+		list.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う", true));
+		list.add(new Task(LocalDate.of(2021,  9, 15), "○○社面接", false));
+		list.add(new Task(LocalDate.of(2021, 12,  4), "手帳を買う", true));
+		list.add(new Task(LocalDate.of(2021,  8, 10), "散髪に行く", false));
+		list.add(new Task(LocalDate.of(2021, 11,  9), "スクールの課題を解く", false));
+		
+		long number = list.stream().filter(t -> t.isDone() == false).count();
+		System.out.println("未完了のタスクの個数は" + number + "\n【未完了のタスクを昇順に並び替えて一覧表示】");
+		list.stream().filter(t -> t.isDone() == false).sorted().forEach(System.out::println);
+		
+	}
+}
---
+++ src/Task.java
@@ -0,0 +1,37 @@
+
+import java.time.LocalDate;
+
+
+public class Task implements Comparable<Task> {
+	private LocalDate date;
+	private String task;
+	private boolean done;
+	
+	public Task(LocalDate date, String task, boolean done) {
+		this.date = date;
+		this.task = task;
+		this.done = done;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public String getTask() {
+		return task;
+	}
+	
+	public boolean isDone() {
+		return done;
+	}
+	
+	@Override
+	public int compareTo(Task t) {
+		return this.date.compareTo(t.date);
+	}
+	
+	@Override
+	public String toString() {
+		return this.date + "" + this.task;
+	}
+}
